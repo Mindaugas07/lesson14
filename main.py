943ab7d622d54092063e5f0b913f1fa0363a8688
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-19 20:08:52 +0300

    modify file main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # 2. Create a function that adds a string ending to each member in a list.
-from typing import List, Union 
-# def add_string(string: str, list: List[Union[float,int, str]]) -> list:
-#     return [str(member) + string for member in list]
+# from typing import List, Union 
+# def add_string(some_letters: str, list_of_varablies: [float,int, str]) -> list [float, int, str]:
+#     return [str(member) + some_letters for member in list_of_varablies]
 
 # print(add_string("sdf", [1, 2.4, "weqr"]))
 
 # 4. Create a function that returns only strings with unique characters.
 
-def string_with_unique_char()
+# def string_with_unique_char(string: str) -> list [str]:
+#     special_characters = "$%^&*()-+?_=,<>/@"
+#     splited_string = string.split(" ")
+#     words_with_spec_chars = [word for word in splited_string if set(special_characters).intersection(word)]
+       
+#     return words_with_spec_chars
+        
+# print(string_with_unique_char("asdf 466%54 sadf: @dsaf %"))
+

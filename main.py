9bb5500509a9d1ac473f175886d798579610d130
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-19 18:57:00 +0300

    add file main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# 2. Create a function that adds a string ending to each member in a list.
+from typing import List, Union 
+# def add_string(string: str, list: List[Union[float,int, str]]) -> list:
+#     return [str(member) + string for member in list]
+
+# print(add_string("sdf", [1, 2.4, "weqr"]))
+
+# 4. Create a function that returns only strings with unique characters.
+
+def string_with_unique_char()

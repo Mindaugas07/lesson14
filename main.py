2d5688338ce5f0a8fcf56a57e05748d61ed73522
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-22 17:59:40 +0300

    modify main.py file

--- main.py
+++ main.py
@@ -27,13 +27,29 @@
 #     ten_random_numbers_list = [(randint(1, 100)) for i in range(10)]
 #     return ten_random_numbers_list
 
-# input_string = input("Please enter 10 random integers from 1 to 100: ")
 
 # def calculate_sum_of_numbers(list_of_ten_random_numbers: list) -> int:
 #     return sum(list_of_ten_random_numbers)
 
 # print(calculate_sum_of_numbers(ten_random_numbers_list))
 
+def ten_random_integers_from_input() -> list:
+    input_string = input("Please enter 10 random integers from 1 to 100 in one string as '1, 6, 18, 45, 99 ...etc' ")
+    ten_random_number_list = [int(number) for number in input_string.split(",")]
+    return(ten_random_number_list)
+
+# def multiplication_highest_lowest_number_from_list(ten_random_integers_from_input: list) -> int:
+#     ten_interger_list = ten_random_integers_from_input() 
+#     return max(ten_interger_list) * min(ten_interger_list)
+
+# print(multiplication_highest_lowest_number_from_list(ten_random_integers_from_input))
+
+def numbers_of_string_in_opposite_order(ten_random_integers_from_input: list) -> list:
+    ten_integer_list = ten_random_integers_from_input()
+    ten_integer_list.sort(reverse=True)
+    return ten_integer_list
+
+print(numbers_of_string_in_opposite_order(ten_random_integers_from_input))
 
 # Task nr.2:
 # Create a program , which takes 3 differnt sentences(ne maziau). The input should have all 
